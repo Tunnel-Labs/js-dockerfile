977e33b39f0aea207a40b50156b54e074c0632a6
Leon Si <leon@leondreamed.com>
2023-04-12 02:25:29 -0400

    chore: upgrade lionconfig

--- packages/js-dockerfile/scripts/build.ts
+++ packages/js-dockerfile/scripts/build.ts
@@ -1,6 +1,5 @@
-import { chProjectDir, copyPackageFiles, tsc, rmDist } from 'lionconfig';
+import { copyPackageFiles, rmDist, tsc } from 'lionconfig'
 
-chProjectDir(import.meta.url)
 rmDist()
 await tsc()
 await copyPackageFiles()
